141a5668edd1837604e1905a7b2808f51f4bc8f9
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-05-11 10:07:00 +0300

    init project

---
+++ src/js/_components.js
@@ -0,0 +1,11 @@
+import './components/iMask'
+import './components/dropzone'
+import './components/customSelect'
+import './components/cutString'
+import './components/manageChangeableLists'
+import './components/slider'
+import './components/customDate'
+import './components/manageModal'
+
+
+
