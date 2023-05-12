032d93b58cbbc8bea808de2cbc610fa61e23329a
Kramskoy-v <kramskoy.va@gmail.com>
2023-05-12 11:57:24 +0300

    стационарка стр profile-teacher(index)

--- src/js/components/profileEditable.js
+++ src/js/components/profileEditable.js
@@ -1,12 +1,20 @@
-const profileDataPage = document.querySelector('.profile-data-page')
+const profileDataPage = document.querySelector('.profile-teacher-page')
 
 if (profileDataPage) {
   const editBtn = profileDataPage.querySelector('.profile-data .edit-btn')
   const editableBlock = profileDataPage.querySelector('.profile-data')
+  const disabledElements = profileDataPage.querySelectorAll('.profile-form__disabled')
+  const cancelBtn = profileDataPage.querySelector('.profile-form__btn-cancel')
 
   editBtn.addEventListener('click', (e) => {
     e.preventDefault()
     editableBlock.classList.add('_edit-active')
+    disabledElements.forEach(elem=> elem.disabled = false)
   })
+
+  cancelBtn.addEventListener('click', ()=> {
+    editableBlock.classList.remove('_edit-active')
+    disabledElements.forEach(elem=> elem.disabled= true)
+  } )
 
 }
