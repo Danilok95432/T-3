a37e496bb7ad26aea864a7042c3081b6f9d900ba
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-03-01 17:45:45 +0300

    index.html: 5 секций главной страницы

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,5 +1,37 @@
-import {Swiper} from "swiper/bundle";
+import { Swiper } from "swiper/bundle";
 
 
 // Инициализация слайдеров
 
+new Swiper(".support-swiper", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  grabCursor: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    1280: {
+      slidesPerView: 5,
+    }
+
+  },
+});
+
+new Swiper(".prospects-swiper", {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 1,
+  grabCursor: true,
+  pagination: {
+    el: ".prospects-swiper-pagination",
+    clickable: true,
+  },
+});
