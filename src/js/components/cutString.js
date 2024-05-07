6ef7ede26cdeaf6ee557b60254664caaa3994298
ludaalt <38361378+ludaalt@users.noreply.github.com>
2024-05-07 18:14:29 +0300

    добавлен адаптив на некоторые страницы студента и преподавателя

--- src/js/components/cutString.js
+++ src/js/components/cutString.js
@@ -1,10 +1,16 @@
-import { cutString } from "../_functions"
+import { cutString } from "../_functions";
 
 //обрезка длинных строк
 
-const allCutStrings = document.querySelectorAll('[data-shear]')
+const allCutStrings = document.querySelectorAll("[data-shear]");
 
 if (allCutStrings) {
-  cutString(allCutStrings)
+  cutString(allCutStrings);
 }
 
+const paymentStrings = document.querySelectorAll(".payment-cut-string");
+const mediaQuery = window.matchMedia("(max-width: 390px)");
+
+if (mediaQuery.matches) {
+  cutString(paymentStrings);
+}
