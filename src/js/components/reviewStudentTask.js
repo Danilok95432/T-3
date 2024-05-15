c85ed8c36308df8e20f0fb2248bf836b7500473d
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-05-15 16:12:31 +0300

    list-tasks.html: правка

--- src/js/components/reviewStudentTask.js
+++ src/js/components/reviewStudentTask.js
@@ -1,60 +1,42 @@
 import {
-    formToObj,
-    sendData,
-    serializeForm,
-    showInfoModal,
-  } from '../_functions'
-
+  formToObj,
+  sendData,
+  serializeForm,
+  showInfoModal,
+} from '../_functions'
 
 const acceptTaskButton = document.querySelector('.accept-task-button')
 const rejectTaskButton = document.querySelector('.reject-task-button')
 const reviewTaskForm = document.querySelector('.review-task-form')
 
+if (reviewTaskForm) {
+  const sendReview = async (e) => {
+    e.preventDefault()
+    const data = serializeForm(reviewTaskForm)
+    const objData = formToObj(data)
+    const resultObj = {
+      ...objData,
+      isAccept: e.target.classList.contains('accept-task-button'),
+      add: e.target.dataset.add,
+    }
+    const jsonData = JSON.stringify(resultObj)
 
-const sendReview =  (button, isAccept) => {
-    button.addEventListener('click', async (e)  => {
-        e.preventDefault()    
-        const data = serializeForm(reviewTaskForm)    
-        const objData = formToObj(data)    
-        const newObj = {
-            ...objData,
-            isAccept: isAccept,    
-            add: button.dataset.add        
-        }    
-        const jsonData = JSON.stringify(newObj)
+    try {
+      const response = await sendData(jsonData, e.target.dataset.url)
+      const finishedResponse = await response.json()
 
-        try {
-            const response = await sendData(jsonData, button.dataset.url)
-            const finishedResponse = await response.json()
-      
-            const { status, errortext, message } = finishedResponse
-            if (status === 'ok') {
-              showInfoModal(message)  
-              // location.reload()
-            } else {
-              showInfoModal(errortext)
-            }
-        } catch (err) {
-            showInfoModal('Во время выполнения запроса произошла ошибка')
-            console.error(err)
-        } 
-    })
+      const { status, errortext, message } = finishedResponse
+      if (status === 'ok') {
+        showInfoModal(message)
+      } else {
+        showInfoModal(errortext)
+      }
+    } catch (err) {
+      showInfoModal('Во время выполнения запроса произошла ошибка')
+      console.error(err)
+    }
+  }
+
+  acceptTaskButton.addEventListener('click', sendReview)
+  rejectTaskButton.addEventListener('click', sendReview)
 }
-
-if (acceptTaskButton) {
-    sendReview(acceptTaskButton)
-}
-
-if (rejectTaskButton) {
-    sendReview(rejectTaskButton)
-}
-
-
-
-
-
-
-
-
-
-
