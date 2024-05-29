224cc962af48f3d54e45870be4a39a81e9ed3ea8
luspi1 <igbabin566@gmail.com>
2024-05-29 11:12:08 +0300

    изменено лого в мобильной версии

--- src/js/components/burgerMenu.js
+++ src/js/components/burgerMenu.js
@@ -6,6 +6,7 @@ if (burgerContainer) {
   const burgerBtn = burgerContainer.querySelector('.burger-btn')
 
   burgerBtn.addEventListener('click', () => {
+    window.scrollTo(0, 0)
     burgerContainer.classList.toggle('_burger-active')
     body.classList.toggle('_block')
   })
