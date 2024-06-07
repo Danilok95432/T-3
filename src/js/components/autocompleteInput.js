1792796a136776c2b0611ba7b6588f90abf6511f
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-06-07 15:22:27 +0300

    сделан компонент autocompleteInput, добавлен на add-report, complete-task

--- src/js/components/autocompleteInput.js
+++ src/js/components/autocompleteInput.js
@@ -2,6 +2,24 @@ import { sendData, showInfoModal } from '../_functions'
 
 const autocompleteInputs = document.querySelectorAll('.autocomplete-input')
 
+const renderSuggestions = (suggestions, input, popup) => {
+  if (suggestions.length > 0) {
+    suggestions.forEach((item) => {
+      const div = document.createElement('div')
+      div.textContent = item
+      div.className = 'autocomplete-input__popup-item'
+      div.addEventListener('click', () => {
+        input.value = item
+        popup.style.display = 'none'
+      })
+      popup.appendChild(div)
+    })
+    popup.style.display = 'block'
+  } else {
+    popup.style.display = 'none'
+  }
+}
+
 if (autocompleteInputs) {
   autocompleteInputs.forEach((inputEl) => {
     const autoField = inputEl.querySelector('input')
@@ -10,20 +28,30 @@ if (autocompleteInputs) {
 
     autoField.addEventListener('input', async (e) => {
       const query = e.currentTarget.value
-      if (query.length < 3) return
+      if (query.length < 3) {
+        autoPopup.style.display = 'none'
+        return
+      }
 
+      autoPopup.innerHTML = ''
       try {
         const response = await sendData(JSON.stringify(query), autoScript)
         const finishedResponse = await response.json()
         const { status, errortext, suggestions } = finishedResponse
         if (status === 'ok') {
-          console.log(suggestions)
+          renderSuggestions(suggestions, autoField, autoPopup)
         } else {
           showInfoModal(errortext)
         }
       } catch (err) {
         showInfoModal('Во время выполнения запроса произошла ошибка')
         console.error(err)
+      }
+    })
+
+    document.addEventListener('click', (event) => {
+      if (!inputEl.contains(event.target)) {
+        autoPopup.style.display = 'none'
       }
     })
   })
