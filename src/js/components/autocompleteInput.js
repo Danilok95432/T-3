e22bc9af24711436ec448b16a8c2350e2ea8fc01
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-06-07 12:52:00 +0300

    начало autocompleteInput

--- src/js/components/autocompleteInput.js
+++ src/js/components/autocompleteInput.js
@@ -1,1 +1,30 @@
-const autocompleteInputs = document.querySelector('.autocomplete-input')
+import { sendData, showInfoModal } from '../_functions'
+
+const autocompleteInputs = document.querySelectorAll('.autocomplete-input')
+
+if (autocompleteInputs) {
+  autocompleteInputs.forEach((inputEl) => {
+    const autoField = inputEl.querySelector('input')
+    const autoPopup = inputEl.querySelector('.autocomplete-input__popup')
+    const autoScript = inputEl.dataset.script
+
+    autoField.addEventListener('input', async (e) => {
+      const query = e.currentTarget.value
+      if (query.length < 3) return
+
+      try {
+        const response = await sendData(JSON.stringify(query), autoScript)
+        const finishedResponse = await response.json()
+        const { status, errortext, suggestions } = finishedResponse
+        if (status === 'ok') {
+          console.log(suggestions)
+        } else {
+          showInfoModal(errortext)
+        }
+      } catch (err) {
+        showInfoModal('Во время выполнения запроса произошла ошибка')
+        console.error(err)
+      }
+    })
+  })
+}
