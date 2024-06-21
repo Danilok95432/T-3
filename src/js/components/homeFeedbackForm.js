3750e95c0ed310851f35149491cef80b174acb8b
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-06-21 12:39:36 +0300

    доработка формы feedback на главной

---
+++ src/js/components/homeFeedbackForm.js
@@ -0,0 +1,35 @@
+import {
+  formToObj,
+  sendData,
+  serializeForm,
+  showInfoModal,
+} from '../_functions'
+
+const feedbackForm = document.querySelector('.home-page .feedback__form')
+
+if (feedbackForm) {
+  const feedbackScript = feedbackForm.action
+  feedbackForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const data = serializeForm(feedbackForm)
+    const objData = formToObj(data)
+
+    const jsonData = JSON.stringify(objData)
+
+    try {
+      const response = await sendData(jsonData, feedbackScript)
+      const finishedResponse = await response.json()
+
+      const { status, errortext, message } = finishedResponse
+
+      if (status === 'ok') {
+        showInfoModal(message)
+      } else {
+        showInfoModal(errortext)
+      }
+    } catch (err) {
+      showInfoModal('Во время выполнения запроса произошла ошибка')
+      console.error(err)
+    }
+  })
+}
