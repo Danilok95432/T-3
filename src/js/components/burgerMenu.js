5b0470deb976c46556c75927b4319d270a544241
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-06-24 16:28:45 +0300

    внедрение нового header на все страницы

--- src/js/components/burgerMenu.js
+++ src/js/components/burgerMenu.js
@@ -6,12 +6,12 @@ if (burgerMenu) {
   const burgerOpenBtn = document.querySelector('.burger-btn._open')
   const burgerCloseBtn = document.querySelector('.burger-btn._close')
 
-  burgerOpenBtn.addEventListener('click', () => {
+  burgerOpenBtn?.addEventListener('click', () => {
     window.scrollTo(0, 0)
     burgerMenu.classList.add('_active')
     body.classList.add('_block')
   })
-  burgerCloseBtn.addEventListener('click', () => {
+  burgerCloseBtn?.addEventListener('click', () => {
     burgerMenu.classList.remove('_active')
     body.classList.remove('_block')
   })
